765866760e73c6f9194dc229e3ab5d85e7071306
maskedsyntax <aftaab2507@gmail.com>
2025-09-08 23:32:51 +0530

    refactor: remove unused conditional statements

--- js/main.js
+++ js/main.js
@@ -20,21 +20,11 @@ document.addEventListener("DOMContentLoaded", function () {
       e.preventDefault();
       const target = link.getAttribute("href");
 
-      let defaultContent = document.querySelector("#content").innerHTML;
       let contentHTML;
       if (target === "/") {
-        // contentHTML = defaultContent;
         const res = await fetch("pages/home.html");
         contentHTML = await res.text();
         window.history.pushState({ page: "home" }, "Home", "/");
-      } else if (target === "/Mission") {
-        const res = await fetch("pages/mission.html");
-        contentHTML = await res.text();
-        window.history.pushState({ page: "mission" }, "Mission", "/mission");
-      } else if (target === "/Projects") {
-        const res = await fetch("pages/projects.html");
-        contentHTML = await res.text();
-        window.history.pushState({ page: "projects" }, "Projects", "/projects");
       } else if (target === "/Blogs") {
         const res = await fetch("pages/blogs.html");
         console.log(target);
